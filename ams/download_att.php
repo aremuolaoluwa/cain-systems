3af2ad6933b08d6c61def498d0298491f2b0f392
aremuolaoluwa <aremujnr@gmail.com>
2024-07-26 15:55:55 +0100

    changes

--- ams/download_att.php
+++ ams/download_att.php
@@ -11,8 +11,10 @@
         <div class="form-items">
             <h2>Download Attendance</h2>
             <form action="./include/download.php" method="GET">
-                <label for="date" id="attendance_date">Select Date</label>
-                <input type="date" id="att-date" name="date" required>
+                <label for="start_date">Start Date</label>
+                <input type="date" id="start_date" name="start_date" required>
+                <label for="end_date">End Date</label>
+                <input type="date" id="end_date" name="end_date" required>
                 <div class="button-container">
                     <input type="submit" value="Download">
                 </div>
@@ -20,9 +22,10 @@
         </div>
     </div>
     <footer class="footer-nav-bar">
-        <a href="index.php">Home |</a>
-        <a href="registered_std.php">Registered Students |</a>
-        <a href="mark_attendance.php">Mark Attendance |</a>
+        <a href="index.php">Home | </a>
+        <a href="registered_std.php">Registered Students | </a>
+        <a href="enrolled_students.php">Enrolled Students | </a>
+        <a href="mark_attendance.php">Mark Attendance | </a>
         <a href="download_att.php">Download Attendance</a>
     </footer>
 </body>
