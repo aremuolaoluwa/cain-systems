57998ebbdf2fa519188148916d333aeb61e92817
aremuolaoluwa <aremujnr@gmail.com>
2024-07-30 10:33:35 +0100

    update

--- capp/include/forgot_password.php
+++ capp/include/forgot_password.php
@@ -1,6 +1,11 @@
 <?php
 
-include 'db.php';
+include "db.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailOrUsername = $_POST['email_username'];
@@ -14,19 +19,39 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_result($email);
         $stmt->fetch();
         $resetToken = bin2hex(random_bytes(16));
-        
+
         $stmt = $conn->prepare("UPDATE users SET reset_token = ? WHERE email = ?");
         $stmt->bind_param("ss", $resetToken, $email);
         $stmt->execute();
-        
-        $resetLink = "http://localhost/cms/ams/new_password.php?token=$resetToken";
+
+        $resetLink = "http://localhost/cms/include/new_password.php?token=$resetToken";
         $subject = "Password Reset Request";
-        $message = "Click the following link to reset your password: $resetLink";
-        mail($email, $subject, $message);
-        
-        echo "Password reset link has been sent to your email.";
+        $message = "Click the following link to reset your password: <a href='$resetLink'>$resetLink</a>";
+
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host       = 'smtp.gmail.com';
+            $mail->SMTPAuth   = true;
+            $mail->Username   = 'tech@cainafrica.org';
+            $mail->Password   = '4AllThingsTech!';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port       = 587;
+
+            $mail->setFrom('tech@cainafrica.org', 'CAIN Africa');
+            $mail->addAddress($email);
+
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body    = $message;
+
+            $mail->send();
+            echo "<script>alert('Password reset link has been sent to your email.'); window.location.href='../forgot_password.php';</script>";
+        } catch (Exception $e) {
+            echo "<script>alert('Message could not be sent. Mailer Error: {$mail->ErrorInfo}'); window.location.href='../forgot_password.php';</script>";
+        }
     } else {
-        echo "No user found with that email or username.";
+        echo "<script>alert('No user found with that email or username.'); window.location.href='../forgot_password.php';</script>";
     }
 
     $stmt->close();
